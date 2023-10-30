3f8f9518ef07efc9fdcc1e7198f0568764551c57
Uthayachandran Abineshh <79247690+uabinesh@users.noreply.github.com>
2023-10-30 15:43:51 +0530

    Initial commit

---
+++ App.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import warnings
+warnings.filterwarnings("ignore")
+
+from fertilizerPredict import fertilizerPrediction
+from burnDamage import damage_classify
+
+app = Flask(__name__)
+
+@app.route("/test", methods=['GET'])
+def testing():
+    return 'testing'
+
+@app.route("/fertilizer-predict", methods=['POST'])
+def prediction():
+    output = fertilizerPrediction()
+    return output
+
+@app.route("/damage_classify", methods=['POST'])
+def burnDamage():
+    output = damage_classify()
+    return output
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
---
+++ burnDamage.py
@@ -0,0 +1,90 @@
+#importing necessary libraries
+# from cmath import sin
+from flask import Flask, jsonify, request,render_template
+import pandas as pd
+
+import tensorflow as tf
+from PIL import Image
+import requests
+from io import BytesIO
+import matplotlib.pyplot as plt
+
+
+import pickle
+import numpy as np
+import warnings
+warnings.filterwarnings("ignore")
+
+app = Flask(__name__)
+# Load the trained model from the pickle file
+with open('Damage_Classification_2.pkl', 'rb') as f:
+    damage_classify_model = pickle.load(f)
+
+with open('Fertilizer_Damage_Severity_Classification.pkl', 'rb') as f:
+    fertilizer_burn_classify_model = pickle.load(f)
+
+with open('Pesticide_Damage_Severity_Classification.pkl', 'rb') as f:
+    pesticide_damage_classify_model = pickle.load(f)
+
+def preprocess_image(image):
+    img = image.convert("RGB")  # Convert image to RGB format
+    img = img.resize((256, 256))
+    img_array = tf.keras.preprocessing.image.img_to_array(img)
+    img_preprocessed = tf.expand_dims(img_array, 0)
+    return img_preprocessed
+
+def testing():
+    return 'testing'
+
+def burn_level(type , url):
+
+    print("burn_level started")
+    image_url = url
+
+    class_names = ['100', '20', '40', '60', '80']
+    
+    # Access the value passed in the URL via the 'value' parameter
+    
+    uploaded_image_url = image_url
+    #imge from url
+    response = requests.get(uploaded_image_url)
+    image = Image.open(BytesIO(response.content))
+    uploaded_image = preprocess_image(image)
+
+    if(type==0):
+        prediction = fertilizer_burn_classify_model.predict(np.array(uploaded_image))
+    else:
+        prediction = pesticide_damage_classify_model.predict(np.array(uploaded_image))
+    print(prediction)
+    top=np.argmax(prediction[0])
+    print(class_names[top])
+    return (class_names[top])
+
+def damage_classify():
+    json = request.get_json()
+    print(json)
+
+    print("asdt predict")
+    image_url = request.get_json().get('url')
+    # .args.get('url')
+    class_names = ['Tomato_fertilizer_burn', 'Tomato_healthy', 'Tomato_pesticide_burn']
+    
+    # Access the value passed in the URL via the 'value' parameter
+    
+    uploaded_image_url = image_url
+    #imge from url
+    response = requests.get(uploaded_image_url)
+    image = Image.open(BytesIO(response.content))
+    uploaded_image = preprocess_image(image)
+
+    prediction = damage_classify_model.predict(np.array(uploaded_image))
+    print(prediction)
+    top=np.argmax(prediction[0])
+    print(class_names[top])
+    if((top==0)|(top==2)):
+        level=burn_level(top,uploaded_image_url)
+
+    return ({"Prediction":str(top), "Level":level})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
